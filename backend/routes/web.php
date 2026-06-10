f704f25ec840e4bbd2d46f8726584d313fed18af
Danie1l6 <danielsierra103@gmail.com>
2026-06-10 14:00:31 -0500

    pruebas traductor

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -8,3 +8,15 @@ Route::get('/', function () {
         'message' => 'Gym API is running.',
     ]);
 });
+
+Route::get('/test-translate', function () {
+
+    $response = Http::post('http://localhost:5000/translate', [
+        'q' => 'Lie flat on your back.',
+        'source' => 'en',
+        'target' => 'es',
+        'format' => 'text'
+    ]);
+
+    return $response->json();
+});
